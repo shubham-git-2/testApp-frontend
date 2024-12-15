7a17300983ccc56bc804cd59d9a90546c965979e
Shubham Sharma <shubham.sharma@sifars.com>
2024-12-15 08:51:31 +0530

    add signup page endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './Home.tsx'
+import SignupPage from "./pages/SignupPage.tsx"
+import UserCreateAction from "./action/user/create.tsx"
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home/>,
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />,
+    },
+    {
+        path: "/users",
+        action: UserCreateAction,
     },
 ]);
 
